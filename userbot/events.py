5f420a71de7a5a4d0ba5029347c38996cdc34868
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-12 17:23:51 +0300

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# SiriUserBot - Berce
+# LoungeUserBot - Bodrumlubebek % SakirBey
 
 """ Olayları yönetmek için UserBot modülü.
  UserBot'un ana bileşenlerinden biri. """
