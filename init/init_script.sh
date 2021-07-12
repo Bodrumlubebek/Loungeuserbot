49e679eb597f2efdf77841cb5bb794898e0e913f
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-12 14:47:51 +0300

    Update init_script.sh

--- init/init_script.sh
+++ init/init_script.sh
@@ -48,7 +48,7 @@ botclone() {
     cd ~
     echo "Cloning bot sources..."
     if [ -z "$bot_clone" ]; then
-        git clone https://github.com/ErdemBey1/SiriUserBot -b master
+        git clone https://github.com/Bodrumlubebek/Loungeuserbot -b main
         save_prog "bot_clone"
     fi
     echo "DONE!!"
