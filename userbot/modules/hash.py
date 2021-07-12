a15c7fc939d57c4e83aa5831938e7cc4c722bd8f
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-12 21:52:21 +0300

    Update hash.py

--- userbot/modules/hash.py
+++ userbot/modules/hash.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# SiriUserBot - ErdemBey - Midy
+# LoungeUserBot - SakirBey - Bodrumlubebekk
 
 
 """ Hash ve encode/decode çözme komutlarını içeren UserBot modülü. """
