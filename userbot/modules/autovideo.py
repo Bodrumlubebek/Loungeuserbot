d47f2a7d4d952a21d5c1c0ba4f11bc79b8197e6d
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-12 21:10:57 +0300

    Update autovideo.py

--- userbot/modules/autovideo.py
+++ userbot/modules/autovideo.py
@@ -1,14 +1,14 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020 SakirBey.
 #
 # Licensed under the  GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# SiriUserBot - ErdemBey - Berce
+# LoungeUserBot - SakirBey - Bodrumlubebekk
 
-# @NaytSeyd tarafından portlanmıştır.
-# @frknkrc44 tarafından düzenlenmiştir.
-# @Fusuf tarafından AutoVideo yazılmıştır.
+# @Sakirhackofficial90 tarafından portlanmıştır.
+# @Sakirhackofficial99 tarafından düzenlenmiştir.
+# @Bodrumlubebekk tarafından AutoVideo yazılmıştır.
 
 import os
 from datetime import datetime
